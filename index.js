7ebb26b7cc0d3926d806fc481c4edbb8e42a37e5
Some One <someone@somewhere.com>
2023-12-20 00:35:04 +0100

    Modify index.html, index.js and index.ejs

--- index.js
+++ index.js
@@ -17,7 +17,12 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
 
 // Creating GET request to render homepage and axios GET request to generate random Chuck Norris secret
-app.get("/", async (req, res) => {
+app.get("/", (req, res) => {
+
+    res.sendFile("c:/Users/Armin/Desktop/Training/Web Dev training/BackEnd/Chuck Norris API/index.html");
+});
+
+app.get("/random", async (req, res) => {
     const result = await axios.get(API_URL, config);
     const response = result.data;
     res.render("index.ejs", {secret: response.value, icon: response.icon_url});
