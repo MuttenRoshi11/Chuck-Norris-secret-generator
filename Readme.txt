78f437703a6c1b1a920f4d407208d3322b76e656
Some One <someone@somewhere.com>
2023-12-20 00:04:28 +0100

    Initial commit on main branch

---
+++ Readme.txt
@@ -0,0 +1,14 @@
+Welcome to my first open source project. 
+
+Steps to take:
+
+1. Clone the files to your local machine by using Git clone command. 
+2. Navigate to your desired path where files are cloned.
+3. By using "npm i" install all required dependencies.
+4. Modify code to add some originality.
+5. Final step is to run index.js with ndemon by using command "nodemon index.js"
+
+
+This project is created mainly for learning and fun, I am sure it is lacking on many fields. But it is reflecting my progress so I am happy to share it with everyone. 
+
+I would really appreciate some constructive critics.
---
+++ index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import bodyParser from "body-parser";
+import axios from "axios";
+
+// Creating app object with express as well as API URL and config object with API key
+const app = express();
+const port = 3000;
+const API_URL = "https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/jokes/random"
+const config = { 
+    headers: {
+    accept: 'application/json',
+    'X-RapidAPI-Key': '68b87ad03fmsh6ef4e0597ce7701p12fbb4jsncc5c1346b6f7',
+}};
+
+app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+// Creating GET request to render homepage and axios GET request to generate random Chuck Norris secret
+app.get("/", async (req, res) => {
+    const result = await axios.get(API_URL, config);
+    const response = result.data;
+    res.render("index.ejs", {secret: response.value, icon: response.icon_url});
+});
+
+// Created listening port
+app.listen(port, () => {
+    console.log("Listening on port " + port);
+});
